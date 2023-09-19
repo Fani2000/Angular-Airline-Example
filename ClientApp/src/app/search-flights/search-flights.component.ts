2983ba4cf6052a1ebf91577be8a1ec25c2fdb896
fkeorapetse <fkeorapetse@quickloc8.co.za>
2023-09-19 05:20:04 +0200

    Added the search Flights UI which will later be replaced by form

--- ClientApp/src/app/search-flights/search-flights.component.ts
+++ ClientApp/src/app/search-flights/search-flights.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-search-flights',
   templateUrl: './search-flights.component.html',
-  styleUrls: ['./search-flights.component.css']
+  styleUrls: ['./search-flights.component.css'],
 })
 export class SearchFlightsComponent {
-
+  searchResults: any = [
+    'American Airlines',
+    'British Airways',
+    'South African Airways',
+    'Lufthansa',
+  ];
 }
